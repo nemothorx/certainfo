9c14b3639300bcc6354149f5a1384013d2d23126
Nemo Thorx <nemo@thorx.net>
2022-07-01 21:56:50 +1000

    Cleanup of a maturing script  - Added help option  - Added structure for alt ports  - https headers first  - Output now targets markdown
    modified:  ssl-chaincheck.sh  | 48 +++++++++++++++++++++++++++------------
     1 file changed, 34 insertions(+), 14 deletions(-)

--- ssl-chaincheck.sh
+++ ssl-chaincheck.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-host=${1:-www.google.com}
+host=${1:-help}
 port=${2:-443}
+
+case $host in 
+    help|-h|--help) 
+        echo "
+Print the details of every cert in a chain on a remote server, 
+not just the first (as is openssl s_client default). 
+
+Handles any port with SSL. Defaults to 443. 
+If port is 443, will also give https headers for the server. 
+
+Output is markdown. (acceptable conversion to html via pandoc) 
+
+Usage: $(basename $0) server [port]
+    "
+    exit 0
+    ;;
+
+esac
 
 
 do_chainget() {
     # get info from each cert in a chain
-    # other useful options:
-    #   -serial
-    awk 'BEGIN { pipe="openssl x509 -noout -subject -dates -serial -fingerprint -md5 -issuer"}
+    # md5 fingerprint is for fetchmail needs, otherwise it defaults to SHA1
+    awk 'BEGIN { pipe="openssl x509 -noout -subject -dates -serial -fingerprint -md5  -issuer"}
       /BEGIN CERT/ { count++ ; printf count".\n" }
       /^-+BEGIN CERT/,/^-+END CERT/ { print | pipe }
-      /^-+END CERT/                 { close(pipe) }' # | tr "\n" "|" | sed -e 's/|~|/~/g ; s/issuer/\n    issuer/g' | tr "~" "\n" | column -t -s'|'
+      /^-+END CERT/                 { close(pipe)  }'
 }
 
-echo "# Certificate information for $host:$port as of $(date)"
-openssl s_client -connect $host:$port  -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (!/\.$/) { printf "        " } ; { print } }'
-
+echo "# Full cert chain for $host:$port on: $(date)"
 echo ""
-# TODO: this assumes https. but it should be smarter so we can do IMAPS, POP3S, etc
-if [ "$port" == "443" ] ; then
-    echo "# Additionally: HTTP headers for $host:$port"
-    curl -k -I https://$host:$port
-    echo ""
-fi
+
+# extra info for some ports
+case $port in
+    443)
+        echo "## HTTP headers"
+        curl -s -k -I https://$host:$port | sed -e 's/^/    /g'
+        ;;
+esac
+
+echo "## Certificates"
+openssl s_client -connect $host:$port -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (/=/) { printf "        " } ; { print } }'
