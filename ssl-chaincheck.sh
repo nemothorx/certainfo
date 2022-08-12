f45a3ace87e9079b02d84f2c80f1ca5f4eb16558
Nemo Thorx <nemo@thorx.net>
2022-08-12 22:38:11 +1000

    Improved output.  - http AND https headers (last).  - Help shows pandoc conversion examples
    modified:  ssl-chaincheck.sh  | 27 ++++++++++++++++++++++-----
     1 file changed, 22 insertions(+), 5 deletions(-)

--- ssl-chaincheck.sh
+++ ssl-chaincheck.sh
@@ -12,7 +12,12 @@ not just the first (as is openssl s_client default).
 Handles any port with SSL. Defaults to 443. 
 If port is 443, will also give https headers for the server. 
 
-Output is markdown. (acceptable conversion to html via pandoc) 
+Output is markdown. (and tested for conversion with pandoc) 
+eg:
+$0 uuuuuu.au | pandoc -f markdown -o uuuuuu.au-ssl.html
+$0 uuuuuu.au | pandoc -f markdown --pdf-engine=xelatex -o ssl.pdf
+$0 uuuuuu.au | pandoc -f markdown --pdf-engine=pdfroff -o uuuuuu.au-ssl.pdf
+$0 uuuuuu.au | pandoc -f markdown --pdf-engine=xelatex -V geometry:'top=2cm, bottom=1.5cm, left=2cm, right=2cm' -o uuuuuu.au-ssl.pdf
 
 Usage: $(basename $0) server [port]
     "
@@ -31,16 +36,28 @@ do_chainget() {
       /^-+END CERT/                 { close(pipe)  }'
 }
 
-echo "# Full cert chain for $host:$port on: $(date)"
+echo "# Full cert chain for $host:$port"
+echo "Generated on: $(date)"
+echo ""
+
+echo "## Certificates"
+openssl s_client -connect $host:$port -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (/=/) { printf "        " } ; { print } }'
+
 echo ""
 
 # extra info for some ports
 case $port in
     443)
+        echo "# Additional info"
+        echo ""
+
         echo "## HTTP headers"
-        curl -s -k -I https://$host:$port | sed -e 's/^/    /g'
+        echo ""
+        curl -s -k -I http://$host/ | sed -e 's/^/    /g'
+
+        echo "## HTTPS headers"
+        echo ""
+        curl -s -k -I https://$host:$port/ | sed -e 's/^/    /g'
         ;;
 esac
 
-echo "## Certificates"
-openssl s_client -connect $host:$port -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (/=/) { printf "        " } ; { print } }'
