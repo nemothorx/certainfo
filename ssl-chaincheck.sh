46643719becb6709f35704f0225339309e2a30f3
Nemo Thorx <nemo@thorx.net>
2022-10-29 20:45:47 +1000

    Added cert file ability and port refinements  - Can now inspect local pem/crt files  - Automatically uses -starttls for smtp/pop3/imap ports (25,110,143)
    modified:  ssl-chaincheck.sh  | 77 ++++++++++++++++++++++++++++-----------
     1 file changed, 56 insertions(+), 21 deletions(-)

--- ssl-chaincheck.sh
+++ ssl-chaincheck.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-host=${1:-help}
+extopts=""
+
+if [ -z "$1" ] ; then
+    echo "Please give a .pem/.crt file, or domain"
+    exit 1
+fi
+
+certat=${1:-help}
 port=${2:-443}
 
-case $host in 
+case $certat in 
     help|-h|--help) 
         echo "
 Print the details of every cert in a chain on a remote server, 
@@ -20,6 +27,7 @@ $0 uuuuuu.au | pandoc -f markdown --pdf-engine=pdfroff -o uuuuuu.au-ssl.pdf
 $0 uuuuuu.au | pandoc -f markdown --pdf-engine=xelatex -V geometry:'top=2cm, bottom=1.5cm, left=2cm, right=2cm' -o uuuuuu.au-ssl.pdf
 
 Usage: $(basename $0) server [port]
+   or: $(basename $0) /path/to/cert.pem (or .crt)
     "
     exit 0
     ;;
@@ -36,28 +44,55 @@ do_chainget() {
       /^-+END CERT/                 { close(pipe)  }'
 }
 
-echo "# Full cert chain for $host:$port"
-echo "Generated on: $(date)"
-echo ""
 
-echo "## Certificates"
-openssl s_client -connect $host:$port -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (!/\.$/) { printf "        " } ; { print } }'
+if [ -e ${certat} ] ; then
+    # we're inspecting a local file
+    echo "# Cert inspection of file: ${certat}"
+    echo "Generated on: $(date)"
+    echo ""
+    cat "$certat" | do_chainget | awk '{ if (!/\.$/) { printf "        " } ; { print } }'
+    echo ""
+    echo "# Additional info"
+    echo ""
+    echo "$HOSTNAME:$(readlink -f ${certat})"
+    echo "$(ls -l "$(readlink -f "${certat}")")"
+else
+    # we're loading cert from online:
+    echo "# Cert inspection of URI: $certat:$port"
+    case $port in
+        25) 
+            extopts="-starttls smtp" 
+            ;;
+        110)
+            extopts="-starttls pop3" 
+            ;;
+        143)
+            extopts="-starttls imap" 
+            ;;
+    esac
+    [ -n "$extopts" ] && echo -e "Cert on port $port obtained using \`$extopts\` option\n"
+    echo "Generated on: $(date)"
 
-echo ""
+    echo ""
+    echo "## Certificates"
+    echo ""
+    openssl s_client -connect $certat:$port -servername $certat $extopts -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (!/\.$/) { printf "        " } ; { print } }'
 
-# extra info for some ports
-case $port in
-    443)
-        echo "# Additional info"
-        echo ""
+    echo ""
 
-        echo "## HTTP headers"
-        echo ""
-        curl -s -k -I http://$host/ | sed -e 's/^/    /g'
+    # extra info for some ports
+    case $port in
+        443)
+            echo "# Additional info"
+            echo ""
 
-        echo "## HTTPS headers"
-        echo ""
-        curl -s -k -I https://$host:$port/ | sed -e 's/^/    /g'
-        ;;
-esac
+            echo "## HTTP headers"
+            echo ""
+            curl -s -k -I http://$certat/ | sed -e 's/^/    /g'
 
+            echo "## HTTPS headers"
+            echo ""
+            curl -s -k -I https://$certat:$port/ | sed -e 's/^/    /g'
+            ;;
+    esac
+fi
