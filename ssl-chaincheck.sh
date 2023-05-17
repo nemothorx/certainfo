b0e99560017e118994832e23939d2f37e7cbb0ab
Nemo Thorx <nemo@thorx.net>
2023-05-17 19:00:58 +1000

    this fork adds subjectAltName, tweaks formatting. SSL before https headers
    modified:  ssl-chaincheck.sh  | 25 ++++++++++++++-----------
     1 file changed, 14 insertions(+), 11 deletions(-)

--- ssl-chaincheck.sh
+++ ssl-chaincheck.sh
@@ -8,19 +8,22 @@ do_chainget() {
     # get info from each cert in a chain
     # other useful options:
     #   -serial
-    awk 'BEGIN { pipe="openssl x509 -noout -subject -dates -issuer"}
-      /BEGIN CERT/ { count++ ; printf "|"count": " }
+    awk 'BEGIN { pipe="openssl x509 -noout -subject -dates -serial -fingerprint -md5 -ext subjectAltName -issuer"}
+      /BEGIN CERT/ { count++ ; printf count".\n" }
       /^-+BEGIN CERT/,/^-+END CERT/ { print | pipe }
-      /^-+END CERT/                 { close(pipe); printf("~")}' | grep -v notBefore | tr "\n" "|" | sed -e 's/|~|/~/g' | tr "~" "\n" | column -t -s'|'
+      /^-+END CERT/                 { close(pipe) }' 
 }
 
-date
+echo "# Certificate information for $host:$port as of $(date)"
+host $host
 echo ""
+openssl s_client -connect $host:$port  -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | awk '{ if (!/\.$/) { printf "        " } ; { print } }' | fmt -s # sed -e 's/, DNS/,\n            DNS/g'
+echo ""
+
+
 # TODO: this assumes https. but it should be smarter so we can do IMAPS, POP3S, etc
-#if [ "$port" == "443" ] ; then
-#    echo "# HTTP headers for $host:$port"
-#    curl -k -I https://$host:$port
-#    echo ""
-#fi
-echo "# Certificate information for $host:$port"
-openssl s_client -connect $host:$port  -servername $host -showcerts </dev/null 2>/dev/null | do_chainget | sed -e "s/^/  /g"
+if [ "$port" == "443" ] ; then
+    echo "# HTTP headers for $host:$port"
+    echo ""
+    curl -s -k -I https://$host:$port
+fi
