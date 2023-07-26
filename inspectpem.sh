9e034ea9e31180193a75fa18758346584807dfd3
Nemo Thorx <nemo@thorx.net>
2023-07-26 21:58:26 +1000

    Minimising code size: 206 bytes / 4 lines
    modified:  inspectpem.sh  | 10 +++-------
     1 file changed, 3 insertions(+), 7 deletions(-)

--- inspectpem.sh
+++ inspectpem.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-echo ""
-
-cat $1 | awk 'BEGIN { pipe="openssl x509 -noout -subject -dates -serial -fingerprint -md5 -ext subjectAltName -issuer"}
-      /BEGIN CERT/ { count++ ; printf count".\n"}
-      /^-+BEGIN CERT/,/^-+END CERT/ { print | pipe }
-      /^-+END CERT/                 { close(pipe); printf("\n")}'  | awk '{ if (!/\.$/) { printf "        " } ; { print } }' | fmt -s
+cat $1 | awk 'BEGIN { ossl="openssl x509 -noout -subject -dates -serial -ext subjectAltName -issuer"}
+/^-+BEGIN CERT/,/^-+END CERT/ { print | ossl }
+/^-+END CERT/  { close(ossl); printf("\n")}'
