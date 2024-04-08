c084be492b19209b56f950a57661826917f430fc
Nemo Thorx <nemo@thorx.net>
2024-04-08 12:41:22 +1000

    alternate openssl pipe option sets in comments. Output tweaks
    modified:  inspectpem.sh  | 7 +++++--
     1 file changed, 5 insertions(+), 2 deletions(-)

--- inspectpem.sh
+++ inspectpem.sh
@@ -2,7 +2,10 @@
 
 echo ""
 
-cat $1 | awk 'BEGIN { pipe="openssl x509 -noout -subject -dates -serial -fingerprint -md5 -ext subjectAltName -issuer"}
+#cat $1 | awk 'BEGIN { pipe="openssl x509 -noout -text -subject -serial -fingerprint -md5 -dates -issuer -certopt no_header,no_version,no_serial,no_signame,no_validity,no_pubkey,no_sigdump"}
+#cat $1 | awk 'BEGIN { pipe="openssl x509 -noout -subject -serial -fingerprint -md5 -dates -issuer"}
+#cat $1 | awk 'BEGIN { pipe="openssl x509 -noout -subject -serial -fingerprint -dates -issuer"}
+cat $1 | awk 'BEGIN { pipe="openssl x509 -in /dev/stdin -noout -subject -ext subjectAltName -serial -fingerprint -dates -issuer"}
       /BEGIN CERT/ { count++ ; printf count".\n"}
       /^-+BEGIN CERT/,/^-+END CERT/ { print | pipe }
-      /^-+END CERT/                 { close(pipe); printf("\n")}'  | awk '{ if (!/\.$/) { printf "        " } ; { print } }' | fmt -s
+      /^-+END CERT/                 { close(pipe); printf("\n")}'  | awk '{ if (!/\.$/) { printf "        " } ; { print } }'
