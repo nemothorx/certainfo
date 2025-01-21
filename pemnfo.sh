eaf70311271348086c7d822edb525b838e85ebe2
Nemo Thorx <nemo@thorx.net>
2025-01-21 20:35:42 +1000

    pemnfo now has sha256 fingerprint. pemnfo_2 removed
    modified:  pemnfo.sh    |  2 +-
    deleted:   pemnfo_2.sh  | 11 -----------
     2 files changed, 1 insertion(+), 12 deletions(-)

--- pemnfo.sh
+++ pemnfo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat $1 | awk 'BEGIN { ossl="openssl x509 -noout -subject -dates -serial -ext subjectAltName -issuer"}
+cat $1 | awk 'BEGIN { ossl="openssl x509 -noout -subject -dates -serial -fingerprint -sha256 -ext subjectAltName -issuer"}
 /^-+BEGIN CERT/,/^-+END CERT/ { print | ossl }
 /^-+END CERT/  { close(ossl); printf("\n")}'
