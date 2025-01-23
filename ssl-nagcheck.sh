03135ffd45ba6ff34c7307ae8d7508de03ecce7c
Nemo Thorx <nemo@thorx.net>
2025-01-23 22:32:40 +1000

    ssl-nagcheck add basic content check. Tweak output for brevity
    modified:  ssl-nagcheck.sh  | 27 +++++++++++++++++----------
     1 file changed, 17 insertions(+), 10 deletions(-)

--- ssl-nagcheck.sh
+++ ssl-nagcheck.sh
@@ -33,7 +33,7 @@ fi
 do_termcol() {
     if [ -t ] ; then
         # one sed to add '~' for manual columnnation, and a second for adding colour. This makes tweaking/debugging the two easier
-        cat /dev/stdin | sed -e "s/DNS:/~DNS:/g ; s/EXPIRY:/~EXPIRY:/g" | sed -e "s/OK/${grn}OK${rst}/g ; s/WARNING/${ylw}WARNING${rst}/g ; s/ CRITICAL/ ${red}CRITICAL${rst}/g ; s/UNKNOWN/${mve}UNKNOWN${rst}/g ; s/SUPER-CRITICAL/${red}${rev}SUPER-CRITICAL${rst}/g" | column -t -s~ | sort -t'(' -k3rn
+        cat /dev/stdin | sed -e "s/DNS:/~DNS:/g ; s/EXPIRY:/~EXP:/g" | sed -e "s/OK/${grn}OK${rst}/g ; s/WARNING/${ylw}WARNING${rst}/g ; s/ CRITICAL/ ${red}CRITICAL${rst}/g ; s/UNKNOWN/${mve}UNKNOWN${rst}/g ; s/SUPER-CRITICAL/${red}${rev}SUPER-CRITICAL${rst}/g" | column -t -s~ | sort -t'(' -k3rn
     else
         cat /dev/stdin
     fi
@@ -47,7 +47,7 @@ do_analysis() {
         case $line in
             Not*)   # Not After:
                 expdate=${line#*:}
-                expdate=$(echo $expdate)    # this removes leading/trailing spaces (and collapses any internal)
+                expdate=$(date -I -d "$expdate") # brevity: ISO 8601 date and ignore time
                 now_t=$(date +%s)
                 expdate_t=$(date +%s -d "$expdate")
                 ttl=$(($expdate_t-$now_t))
@@ -76,8 +76,8 @@ do_analysis() {
             serial=*)
                 stmp0=${line#*=}
                 stmp0cnt=${#stmp0}
-                stmp0skip=$((stmp0cnt-5))
-                stmp1=${stmp0:0:5}
+                stmp0skip=$((stmp0cnt-3))
+                stmp1=${stmp0:0:3}
                 stmp2=${stmp0:$stmp0skip:100}
                 serial="${stmp1}[$stmp0skip]${stmp2}"
                 ;;
@@ -86,9 +86,9 @@ do_analysis() {
                 ;;
         esac
     done < <(cat /dev/stdin)
-    echo -n "Serial: $serial"
+    echo -n "S/N: $serial"
     case $expstatus in
-        *CRITICAL*) echo -n "EXPIRY: $expstatus ($((ttl/86400)) days to renewal [$expdate]) " ;;
+        *CRITICAL*) echo -n "EXPIRY: $expstatus ($((ttl/86400)) days [$expdate]) " ;;
         *)          echo -n "EXPIRY: $expstatus ($((ttl/86400)) days to renewal) "          ;;
     esac
     while read dom ; do
@@ -112,10 +112,11 @@ do_analysis() {
 }
 
 
-#### main, effectively
+########################################### MAIN, effectively
+
 filter=${1:-.}
 [ -t ] && echo "Terminal detected. Verbose output. Filtering by $filter"
-while read host ports path content ; do
+while read host ports uripath content ; do
     while read port ; do
         case $port in
             25) opensslopts="-starttls smtp" ;;
@@ -128,9 +129,15 @@ while read host ports path content ; do
         esac
 # todo: should check dates on the whole chain
         rsltmp=$(timeout 25 openssl s_client -connect $host:$port -servername $host $opensslopts -showcerts </dev/null 2>/dev/null | openssl x509 -noout -text -serial -certopt no_header,no_version,no_serial,no_signame,no_issuer,no_pubkey,no_sigdump -noout 2>/dev/null | grep -E 'Not After :|DNS:|serial=' | do_analysis)
-        [ -t ] && echo "Checking $host:$port ~ $rsltmp" | do_termcol 
+        # TODO: this should handle non-443 ports better (check for smtp/imap/etc banner?)
+        if [ -n "$content" ] ; then
+            curl -s -k -L --connect-timeout 2 -m 5 https://$host:$port/$uripath | grep -q "$content" && contmp="DATA: OK" || contmp="DATA: CRITICAL"
+        else
+            contmp="DATA: N/A"
+        fi
+        [ -t ] && echo "Checking $host:$port ~ $rsltmp ~ $contmp" | do_termcol 
         rslt="$rslt
-$host:$port ~ $rsltmp"
+$host:$port ~ $rsltmp ~ $contmp"
     done < <(echo "$ports" | tr "," "\n" )
 done < <(cat $cfgfile | grep $filter | grep -v '#')
 
